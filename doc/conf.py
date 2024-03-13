9a67011a3c19a75e5e7b3c6ff57e01370b6c6f97
engrosamaali91 <engr.osamaali91@gmail.com>
2024-03-13 11:02:36 +0100

    Force push to resolve conflict

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ sys.path.append(os.path.dirname(__file__) + "/..")
 sys.path.append(os.path.dirname(__file__) + "/../recon_surf")
 
 project = "FastSurfer"
-copyright = "2023, FastSurfer Team"
+copyright = "2024, FastSurfer Team"
 author = "FastSurfer Team"
 copyright = f"{date.today().year}, {author}"
 gh_url = "https://github.com/deep-mi/FastSurfer"
